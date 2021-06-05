9c748f345dc515cb2ea9c58da83183d5036debe5
vishnuktr <vishnuk.qss@gmail.com>
2021-06-05 21:57:16 +0530

    second test

--- Test/src/Canvas.java
+++ Test/src/Canvas.java
@@ -3,6 +3,9 @@ public class Canvas{
     public void draw(Shape shape) {
         System.out.println("Drawing the shape!");
         shape.draw();
+        System.out.println("here allows all.");
+        System.out.println("here allows all.");        System.out.println("here allows all.");
+        System.out.println("here allows all.");
 
     }
 
